bad7467eb69fdb4384d609fcfde281a540d5c2b0
barrymurphybhr <barry.murphy@brighthr.com>
2026-04-09 16:31:11 +0100

    initial commit

---
+++ tests/dashboard.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
+
+test.describe("homepage", () => {
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }, testInfo) => {
+    const username = process.env.PLAYWRIGHT_USERNAME || "";
+    const password = process.env.PLAYWRIGHT_PASSWORD || "";
+
+    await page.goto(`${process.env.BASE_URL}`);
+    await page.getByRole("textbox", { name: "Email address" }).fill(username);
+    await page
+      .getByRole("textbox", { name: "Password visibility" })
+      .fill(password);
+    await page.getByTestId("login-button").click();
+
+    await page.waitForLoadState("networkidle");
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    await testInfo.attach("accessibility-scan-results", {
+      body: JSON.stringify(accessibilityScanResults, null, 2),
+      contentType: "application/json",
+    });
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
